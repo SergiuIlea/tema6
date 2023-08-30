c6e0f146d12e9997d67b1cb92066286b533055fc
SergiuIlea <136497712+SergiuIlea@users.noreply.github.com>
2023-08-30 16:14:52 +0300

    Delete Tema16/Tema16 directory

--- Tema16/Tema16/src/main/java/org/fasttrack/Tema16/Exercitiu/Category.java
+++
@@ -1,7 +0,0 @@
-package org.fasttrack.Tema16.Exercitiu;
-
-public enum Category {
-    ON_SALE,
-    NEW,
-    REFURBISHED;
-}
--- Tema16/Tema16/src/main/java/org/fasttrack/Tema16/Exercitiu/ShopItem.java
+++
@@ -1,7 +0,0 @@
-package org.fasttrack.Tema16.Exercitiu;
-
-public interface ShopItem {
-    String name();
-    int price();
-    Category category();
-}
