dad458abcfddd35048126a43f26880bc94112971
SergiuIlea <136497712+SergiuIlea@users.noreply.github.com>
2023-10-11 12:52:40 +0300

    Delete Tema21/Tema21 directory

--- Tema21/Tema21/src/main/java/org/fasttrack/repository/ActorRepository.java
+++
@@ -1,9 +0,0 @@
-package org.fasttrack.repository;
-
-import org.fasttrack.model.Actor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
-}
--- Tema21/Tema21/src/main/java/org/fasttrack/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package org.fasttrack.repository;
-
-import org.fasttrack.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
